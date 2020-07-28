d35553960c3504c71eb44d040a0e136de3fb2530
Nolan Picini <nolanpic@gmail.com>
2020-07-27 19:48:22 -0700

    Rough in basic input with keyboard toggling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState, useEffect, useRef } from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const COMMAND_KEY = 91
+
+const Summon = (props) => {
+  const [isPrompted, setIsPrompted] = useState(false)
+
+  const summonInput = useRef(null)
+
+  useEffect(() => {
+    const keyListener = (e) => {
+      if (e.keyCode === COMMAND_KEY && e.shiftKey) {
+        // user is pressing both keys, toggle Summon
+        setIsPrompted(!isPrompted)
+      }
+    }
+
+    document.body.addEventListener('keydown', keyListener)
+    document.body.addEventListener('keyup', keyListener)
+
+    // cleanup
+    return () => {
+      document.body.removeEventListener('keydown', keyListener)
+      document.body.removeEventListener('keyup', keyListener)
+    }
+  }, [isPrompted])
+
+  // focus the input
+  useEffect(() => {
+    if (isPrompted) {
+      summonInput.current.focus()
+    }
+  }, [isPrompted])
+
+  return (
+    <React.Fragment>
+      {isPrompted ? (
+        <div className='summon-wrap'>
+          <div className='summon'>
+            <input type='text' placeholder='&rarr;' ref={summonInput} />
+          </div>
+        </div>
+      ) : null}
+    </React.Fragment>
+  )
 }
+
+export default Summon
