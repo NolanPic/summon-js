fa5a4fa87e920ef2863f6edcb5e7a47a2393ff1f
Nolan Picini <nolanpic@gmail.com>
2020-08-07 11:53:04 -0700

    Refactor key listener hook and rough in suggestions

--- src/hooks/useKeyListener.js
+++ src/hooks/useKeyListener.js
@@ -3,7 +3,6 @@ import tinykeys from 'tinykeys'
 
 const useKeyListener = (toggleWithKeys) => {
   const [isPrompted, setIsPrompted] = useState(false)
-  const [summonInput, setSummonInput] = useState('')
 
   useEffect(() => {
     // use the user-defined key combo if available, otherwise default
@@ -17,30 +16,13 @@ const useKeyListener = (toggleWithKeys) => {
       }
     })
 
-    const keyListener = (e) => {
-      // if Summon is already open, start matching suggestions
-      // as the user is typing
-      if (isPrompted && summonInput.length) {
-        setSuggestions(
-          testCommands.filter((cmd) =>
-            cmd.toLowerCase().contains(summonInput.toLowerCase())
-          )
-        )
-      }
-    }
-
-    document.body.addEventListener('keydown', keyListener)
-    document.body.addEventListener('keyup', keyListener)
-
     // cleanup
     return () => {
       unsubscribe()
-      document.body.removeEventListener('keydown', keyListener)
-      document.body.removeEventListener('keyup', keyListener)
     }
   }, [isPrompted])
 
-  return [isPrompted, summonInput, setSummonInput]
+  return [isPrompted]
 }
 
 export default useKeyListener
